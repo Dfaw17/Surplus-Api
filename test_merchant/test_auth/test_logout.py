e4bbc5a8ac2d99d4e68027c859af1e4880ce3d8d
Dfaw17 <dfawwaz17@gmail.com>
2021-03-09 15:37:10 +0700

    add test scenario logout api positive & negative

---
+++ test_merchant/test_auth/test_logout.py
@@ -0,0 +1,67 @@
+import requests
+
+class TestLogout :
+
+    global url_login, url_logut, email, kata_sandi, token
+
+    url_login = "https://staging.adminsurplus.net/api/v2/merchant/auth/login"
+    url_logut = "https://staging.adminsurplus.net/api/v2/merchant/auth/logout"
+    email = "kopiruangvirtual@gmail.com"
+    kata_sandi = "12345678"
+
+    def test_logout_normal (self):
+        param = {
+            "email": email,
+            "password": kata_sandi
+        }
+
+        login = requests.post(url_login, data=param,
+                              headers={'Accept': 'application/json'})
+        token = login.json().get("token")
+        param2 = {
+            "token": token
+        }
+
+        response = requests.post(url_logut, data=param2,
+                                 headers={'Accept': 'application/json'})
+
+        data = response.json()
+        validate_status = data.get('success')
+        validate_message = data.get('message')
+
+        assert validate_status == bool(True)
+        assert response.status_code == 200
+        assert validate_message == "User logged out successfully"
+
+    def test_logout_wrong_token (self):
+
+        param2 = {
+            "token": "WRONG TOKEN !!!"
+        }
+
+        response = requests.post(url_logut, data=param2,
+                                 headers={'Accept': 'application/json'})
+
+        data = response.json()
+        validate_status = data.get('success')
+        validate_message = data.get('message')
+
+        assert validate_status == bool(False)
+        assert response.status_code == 401
+        assert validate_message == "Unauthorized"
+
+    def test_logout_empty_token (self):
+
+        param2 = {
+            "token": "WRONG TOKEN !!!"
+        }
+
+        response = requests.post(url_logut,headers={'Accept': 'application/json'})
+
+        data = response.json()
+        validate_status = data.get('success')
+        validate_message = data.get('message')
+
+        assert validate_status == bool(False)
+        assert response.status_code == 401
+        assert validate_message == "Unauthorized"
