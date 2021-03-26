039a63fabc305d0ed385df4af76c611a661b0312
Dfaw17 <dfawwaz17@gmail.com>
2021-03-26 09:03:34 +0700

    add file bat to auto run test without open the project

---
+++ runserver.bat
@@ -0,0 +1,2 @@
+@echo off
+cmd /k "cd /d C:\Users\dfaww\PycharmProjects\API_Surplus\venv\Scripts & activate & cd /d    C:\Users\dfaww\PycharmProjects\API_Surplus & python manage.py runserver & python -m pytest -v --html-report=./report/report.html"
