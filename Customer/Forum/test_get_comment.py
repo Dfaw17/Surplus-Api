d1944c4abca8ef11ff584ecb9d98baecbcc551b6
Dfaw17 <dfawwaz17@gmail.com>
2021-04-09 13:44:48 +0700

    add test scenario customer forum get comment positive & negative

---
+++ Customer/Forum/test_get_comment.py
@@ -0,0 +1,196 @@
+import requests
+from env import stagging
+from pprint import pprint
+from assertpy import assert_that
+
+class TestCustomerGetCommentForum:
+
+    global setting_env,url_login,get_comment,email,kata_sandi,wrong_token
+
+    setting_env = stagging
+    url_login = f"{setting_env}/api/v2/customer/auth/login/email"
+    get_comment = f"{setting_env}/api/v2/customer/comments/"
+    email = "kopiruangvirtual@gmail.com"
+    kata_sandi = '12345678'
+    wrong_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvc3RhZ2luZy5hZG1pbnN1cnBsdXMubmV0XC9hcGlcL3YyXC9jdXN0b21lclwvYXV0aFwvbG9naW5cL2VtYWlsIiwiaWF0IjoxNjE2ODA1NzI2LCJleHAiOjE2MTkzOTc3MjYsIm5iZiI6MTYxNjgwNTcyNiwianRpIjoib05ESmxFRE5hSzNrN2RtVyIsInN1YiI6NDEyNiwicHJ2IjoiMjc0MTA1ZGE2ZTk1YmVmMjgwNzc4NmRkODczODg2N2NmOWMwMmFhYiJ9.fj51xIfQrqleRvdSJUbWcdrvsxQPUn8HpccnOmTgPDI'
+
+    def test_get_comment_normal(self):
+        param = {
+            'email': email,
+            'password': kata_sandi
+        }
+        headers = {
+            "Accept": "application/json"
+        }
+        login = requests.post(url_login, params=param, headers=headers)
+        headers2 = {
+            "Accept": "application/json",
+            "Authorization": f"Bearer {login.json().get('token')}"
+        }
+        param2 = {
+            'forum_id': '350'
+        }
+        get_commnet = requests.get(get_comment, params=param2, headers=headers2)
+
+        validate_status = get_commnet.json().get('success')
+        validate_message = get_commnet.json().get('message')
+        validate_data = get_commnet.json().get('data')
+
+        assert get_commnet.status_code == 200
+        assert validate_status == bool(True)
+        assert "Komentar ditemukan" in validate_message
+        assert_that(validate_data).is_not_none()
+        assert_that(validate_data[0]).contains_only('id', 'user_id', 'forum_id', 'komentar', 'banyak_like',
+                                                    'created_at',
+                                                    'updated_at', 'is_like', 'time_difference', 'is_report', 'is_post',
+                                                    'commenter', 'commenter_badge')
+
+    def test_get_commnet_none_commnet(self):
+        param = {
+            'email': email,
+            'password': kata_sandi
+        }
+        headers = {
+            "Accept": "application/json"
+        }
+        login = requests.post(url_login, params=param, headers=headers)
+        headers2 = {
+            "Accept": "application/json",
+            "Authorization": f"Bearer {login.json().get('token')}"
+        }
+        param2 = {
+            'forum_id': '349'
+        }
+        get_commnet = requests.get(get_comment, params=param2, headers=headers2)
+
+        validate_status = get_commnet.json().get('success')
+        validate_message = get_commnet.json().get('message')
+
+        assert get_commnet.status_code == 200
+        assert validate_status == bool(True)
+        assert "Komentar ditemukan" in validate_message
+
+    def test_get_commnet_token_empty_value(self):
+        param = {
+            'email': email,
+            'password': kata_sandi
+        }
+        headers = {
+            "Accept": "application/json"
+        }
+        login = requests.post(url_login, params=param, headers=headers)
+        headers2 = {
+            "Accept": "application/json",
+            "Authorization": ""
+        }
+        param2 = {
+            'forum_id': '349'
+        }
+        get_commnet = requests.get(get_comment, params=param2, headers=headers2)
+
+        validate_status = get_commnet.json().get('success')
+        validate_message = get_commnet.json().get('message')
+
+        assert get_commnet.status_code == 401
+        assert validate_status == bool(False)
+        assert "Unauthorized" in validate_message
+
+    def test_get_comment_wrong_param_token(self):
+        param = {
+            'email': email,
+            'password': kata_sandi
+        }
+        headers = {
+            "Accept": "application/json"
+        }
+        login = requests.post(url_login, params=param, headers=headers)
+        headers2 = {
+            "Accept": "application/json",
+            "Authorization": wrong_token
+        }
+        param2 = {
+            'forum_id': '349'
+        }
+        get_commnet = requests.get(get_comment, params=param2, headers=headers2)
+
+        validate_status = get_commnet.json().get('success')
+        validate_message = get_commnet.json().get('message')
+
+        assert get_commnet.status_code == 401
+        assert validate_status == bool(False)
+        assert "Unauthorized" in validate_message
+
+    def test_get_commnet_id_not_found(self):
+        param = {
+            'email': email,
+            'password': kata_sandi
+        }
+        headers = {
+            "Accept": "application/json"
+        }
+        login = requests.post(url_login, params=param, headers=headers)
+        headers2 = {
+            "Accept": "application/json",
+            "Authorization": f"Bearer {login.json().get('token')}"
+        }
+        param2 = {
+            'forum_id': '34999'
+        }
+        get_commnet = requests.get(get_comment, params=param2, headers=headers2)
+
+        validate_status = get_commnet.json().get('success')
+        validate_message = get_commnet.json().get('message')
+
+        assert get_commnet.status_code == 200
+        assert validate_status == bool(True)
+        assert "Komentar ditemukan" in validate_message
+
+    def test_get_commnet_id_text_value(self):
+        param = {
+            'email': email,
+            'password': kata_sandi
+        }
+        headers = {
+            "Accept": "application/json"
+        }
+        login = requests.post(url_login, params=param, headers=headers)
+        headers2 = {
+            "Accept": "application/json",
+            "Authorization": f"Bearer {login.json().get('token')}"
+        }
+        param2 = {
+            'forum_id': 'aaa'
+        }
+        get_commnet = requests.get(get_comment, params=param2, headers=headers2)
+
+        validate_status = get_commnet.json().get('success')
+        validate_message = get_commnet.json().get('message')['forum_id']
+
+        assert get_commnet.status_code == 422
+        assert validate_status == bool(False)
+        assert "Forum harus berupa angka." in validate_message
+
+    def test_get_commnet_id_empty_value(self):
+        param = {
+            'email': email,
+            'password': kata_sandi
+        }
+        headers = {
+            "Accept": "application/json"
+        }
+        login = requests.post(url_login, params=param, headers=headers)
+        headers2 = {
+            "Accept": "application/json",
+            "Authorization": f"Bearer {login.json().get('token')}"
+        }
+        param2 = {
+            'forum_id': ''
+        }
+        get_commnet = requests.get(get_comment, params=param2, headers=headers2)
+
+        validate_status = get_commnet.json().get('success')
+        validate_message = get_commnet.json().get('message')['forum_id']
+
+        assert get_commnet.status_code == 422
+        assert validate_status == bool(False)
+        assert "Forum tidak boleh kosong." in validate_message
--- test_logic.py
+++ test_logic.py
@@ -5,8 +5,7 @@ from assertpy import assert_that
 
 setting_env = stagging
 url_login = f"{setting_env}/api/v2/customer/auth/login/email"
-url_forum = f"{setting_env}/api/v2/customer/forums"
-url_forum_show = f"{setting_env}/api/v2/customer/forums/"
+get_comment = f"{setting_env}/api/v2/customer/comments/"
 email = "kopiruangvirtual@gmail.com"
 kata_sandi = '12345678'
 wrong_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvc3RhZ2luZy5hZG1pbnN1cnBsdXMubmV0XC9hcGlcL3YyXC9jdXN0b21lclwvYXV0aFwvbG9naW5cL2VtYWlsIiwiaWF0IjoxNjE2ODA1NzI2LCJleHAiOjE2MTkzOTc3MjYsIm5iZiI6MTYxNjgwNTcyNiwianRpIjoib05ESmxFRE5hSzNrN2RtVyIsInN1YiI6NDEyNiwicHJ2IjoiMjc0MTA1ZGE2ZTk1YmVmMjgwNzc4NmRkODczODg2N2NmOWMwMmFhYiJ9.fj51xIfQrqleRvdSJUbWcdrvsxQPUn8HpccnOmTgPDI'
@@ -24,25 +23,15 @@ headers2 = {
     "Authorization": f"Bearer {login.json().get('token')}"
 }
 param2 = {
-    'forum_category_id': '1',
-    'perPage': '5',
-    'page': '1'
+    'forum_id': ''
 }
-index_forum = requests.get(url_forum, params=param2, headers=headers2)
-headers3 = {
-    "Accept": "application/json",
-    "Authorization": f"Bearer {login.json().get('token')}"
-}
-show_forum = requests.get(url_forum_show+'aaaa' ,headers=headers3)
+get_commnet = requests.get(get_comment, params=param2, headers=headers2)
 
-validate_status = show_forum.json().get('success')
-validate_message = show_forum.json().get('message')
+validate_status = get_commnet.json().get('success')
+validate_message = get_commnet.json().get('message')['forum_id']
 
-
-assert show_forum.status_code == 404
+assert get_commnet.status_code == 422
 assert validate_status == bool(False)
-assert "Data forum tidak ditemukan" in validate_message
+assert "Forum tidak boleh kosong." in validate_message
 
-
-
-# pprint(show_forum.json())
+# pprint(get_commnet.json())
